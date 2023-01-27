cd5e27b66beded09802897a1f87a0ada2e648651
netomilfont <netomilfont@hotmail.com>
2023-01-27 23:21:42 +0100

    task 2

--- management/product_handler.py
+++ management/product_handler.py
@@ -1,15 +1,25 @@
 from menu import products
 
-def get_product_by_id(id):
+def get_product_by_id(id: int):
     for index, product in enumerate(products):
         if product["_id"] == id:
             return products[index]
     return {}
 
 
-def get_products_by_type(type):
+def get_products_by_type(type: str):
     product_type = []
     for index, product in enumerate(products):
         if product["type"] == type:
             product_type.append(products[index])
     return product_type
+
+
+def add_product(menu: list, **new_product: dict):
+    product_id = 0
+    for product in menu:
+        if product["_id"] > product_id:
+            product_id = product["_id"]
+    new_product["_id"] = product_id + 1
+    menu.append(new_product)
+    return new_product
---
+++ management/tab_handler.py
@@ -0,0 +1,11 @@
+from menu import products
+
+
+def calculate_tab(consum: list):
+    consum_table = 0
+    for product in products:
+        for product_consum in consum:
+            if product["_id"] == product_consum["_id"]:
+                consum_table = product_consum["amount"] * product["price"] + consum_table
+    consum_total = round(consum_table, 2)
+    return {'subtotal': f'{consum_total}'}
